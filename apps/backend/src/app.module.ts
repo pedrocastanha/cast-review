5f7b3cc4500cec1d4bdf90a3607591d3e7970e6d
Pedro CAstanheira Costa <pedrinho.castanha@gmail.com>
2026-08-25 18:37:44 -0300

    feat(backend): adicionar módulo projects ao app.module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -8,6 +8,7 @@ import { AnalysesModule } from './modules/analyses/analyses.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { JwtAccessGuard } from './modules/auth/guards/jwt-access.guard';
 import { BenchmarksModule } from './modules/benchmarks/benchmarks.module';
+import { ProjectsModule } from './modules/projects/projects.module';
 import { RepositoriesModule } from './modules/repositories/repositories.module';
 import { UsersModule } from './modules/users/user.module';
 import { PostgresModule } from './shared/database/postgres/postgres.module';
@@ -30,6 +31,7 @@ import { resolveRedisConnection } from './shared/queue/redis-connection';
     RepositoriesModule,
     AnalysesModule,
     BenchmarksModule,
+    ProjectsModule,
   ],
   controllers: [AppController],
   providers: [
