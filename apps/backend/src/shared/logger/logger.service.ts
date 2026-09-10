e33839b008fd8efa71f68ff1aa5a16d33bbd482c
Pedro CAstanheira Costa <pedrinho.castanha@gmail.com>
2026-09-09 21:57:50 -0300

    refactor(backend): refatore o serviço de logger compartilhado para melhorar a manutenção

--- apps/backend/src/shared/logger/logger.service.ts
+++ apps/backend/src/shared/logger/logger.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, LoggerService } from '@nestjs/common';
-import { initLogger } from 'src/shared/logger/logger.factory';
+import { initLogger } from './logger.factory';
 import { Logger } from 'winston';
 
 type ContextType = Record<string, unknown> & { exception?: unknown };
